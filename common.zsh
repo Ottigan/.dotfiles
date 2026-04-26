23d380bfd8cb128abf6b4281bf852b867759e23a
Janis Malcans <jmalcans@evolution.com>
2026-04-27 01:05:13 +0300

    feat: update how files and directories are handled

--- common.zsh
+++ common.zsh
@@ -18,26 +18,54 @@ export EDITOR="$HOME/.nvim/bin/nvim"
 export VISUAL="$HOME/.nvim/bin/nvim"
 
 # Colorful sudo prompt.
-SUDO_PROMPT="$(tput setaf 2 bold)Password: $(tput sgr0)" && export SUDO_PROMPT
+export SUDO_PROMPT="$(tput setaf 2 bold)Password: $(tput sgr0)"
 
 # FZF configuration.
-export FZF_DEFAULT_COMMAND='fd --type file --color=always'
+export FZF_DEFAULT_COMMAND="fd --type file --color=always"
 export FZF_DEFAULT_OPTS="--height 40% --layout reverse --border --preview 'bat --color=always --style=numbers {}'"
 
-bindkey '^[[Z' autosuggest-accept # shift + tab  | autosuggest
-bindkey '^[[A' history-beginning-search-backward
-bindkey '^P' history-beginning-search-backward
-bindkey '^[[B' history-beginning-search-forward
-bindkey '^N' history-beginning-search-forward
+bindkey "^[[Z" autosuggest-accept # shift + tab  | autosuggest
+bindkey "^[[A" history-beginning-search-backward
+bindkey "^P" history-beginning-search-backward
+bindkey "^[[B" history-beginning-search-forward
+bindkey "^N" history-beginning-search-forward
 
+alias v="nvim"
 alias ls="lsd"
-alias vim="nvim"
 alias lg="lazygit"
 alias config="nvim ~/.zshrc"
-alias vimconfig="nvim ~/.config/nvim"
-alias notes="nvim ~/Documents/Vault/"
 alias sysinfo="fastfetch"
-alias killport='f(){ kill -9 $(lsof -i:$1 -t); unset -f f; }; f'
 alias trim-branches="git branch --merged | grep -v \* | xargs -n 1 git branch -d"
-alias destroy-branches='git branch | grep -Ev "master|develop" | xargs git branch -D'
-alias ff='f(){ find $1 -type f | fzf; unset -f f; }; f'
+alias destroy-branches="git branch | grep -Ev 'master|develop' | xargs git branch -D"
+alias killport=kill-port
+alias vf=fzf-edit-file
+alias vd=fzf-edit-dir
+
+fzf-edit-file() {
+    local file=$(fd --type file --hidden --follow --exclude ".git" . | fzf --preview "bat --color=always --line-range :500 {}")
+    if [[ -n $file ]]; then
+        nvim "$file"
+    fi
+}
+
+fzf-edit-dir() {
+    local dir=$(fd --type directory --hidden --follow --exclude ".git" . | fzf --preview "eza --tree --color=always {} | head -30")
+    if [[ -n $dir ]]; then
+        nvim "$dir"
+    fi
+}
+
+kill-port() {
+    local port=$1
+    if [[ -z $port ]]; then
+        echo "Usage: killport <port>"
+        return 1
+    fi
+    local pid=$(lsof -t -i:$port)
+    if [[ -n $pid ]]; then
+        kill -9 $pid
+        echo "Killed process on port $port (PID: $pid)"
+    else
+        echo "No process found on port $port"
+    fi
+}
